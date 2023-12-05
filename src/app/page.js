bbeaf517879035d35ae9d784ffdd8fac7ede155b
chup-co <trucminion@gmail.com>
2023-12-05 18:44:29 +0700

    sua

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,22 @@
 import Image from 'next/image'
-import Game from './tictactoe/Game'
 import MyApp from '@/tt/Button'
 import AboutPage from '@/tt/AboutPage'
 import Profile from '@/tt/ava'
 import ShoppingList from '@/tt/product'
-
-import Board from './tic-tac-toe/App'
-
+import Game from './tic-tac-toe/Board'
 
 export default function Home() {
 
   return (
 
-      /* <Game></Game>  */
+     
 
       <div>
-       <MyApp></MyApp> 
+      <Game></Game>
+      <MyApp></MyApp> 
       <AboutPage></AboutPage>
       <Profile></Profile>
       <ShoppingList></ShoppingList>
-      <Board></Board>
       </div>
       
   )
